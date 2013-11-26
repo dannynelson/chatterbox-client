580596f44985d32af2d5824f2bf9f8f0f6c4a8f9
Hack Reactor Students <danny.nelson@hackreactor.com>
2013-11-25 16:55:00 -0800

    Made messages work in the right order and stuff

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,27 +1,54 @@
 // YOUR CODE HERE:
 $(document).ready(function() {
   fetch(display);
-  setInterval(function(){ fetch(display); }, 5000);
+  setInterval(function(){
+    fetch(function(data){
+      $('.messages').html('');
+      display(data);
+    });
+  }, 5000);
+
+  $('.submit').on('click', function() {
+    var message = $('.sendBox').val();
+    sendMessage(message);
+    $('.sendBox').val('');
+  });
 });
 
+var server = 'https://api.parse.com/1/classes/chatterbox';
+
+var sendMessage = function(message) {
+  var data = {
+    roomname: "test",
+    text: message,
+    username: username
+  };
+  var test = JSON.stringify(data);
+  $.post(server, JSON.stringify(data));
+};
+
 var fetch = function(callback) {
-  $.get('https://api.parse.com/1/classes/chatterbox', function(data) {
+  $.get(server, { order: "-createdAt" }, function(data) {
     callback(data.results);
   });
 };
 
 var display = function(messages) {
+  messages.sort(function(a, b){
+    return new Date(b.createdAt) - new Date(a.createdAt);
+  });
   var message = '<div class="message"></div>';
   var name = '<div class="name"></div>';
   var text = '<div class="text"></div>';
   var timestamp = '<div class="timestamp"></div>';
 
-  _(messages).each(function(msg) {
+  for(var i = 0; i < 100; i++){
+    var msg = messages[i];
     var messageToAppend = $(message).append(
       $(name).text(msg.username),
       $(text).text(msg.text),
       $(timestamp).text(msg.createdAt)
     );
     $('.messages').append(messageToAppend);
-  });
+  }
 };
