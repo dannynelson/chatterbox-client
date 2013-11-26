1f755aa0a9c43088f4a432eba9688969ea313645
Hack Reactor Students <danny.nelson@hackreactor.com>
2013-11-25 20:24:39 -0800

    Added sidebar with rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   setInterval(function(){
     fetch(function(data){
       $('.messages').html('');
-      display(data);
+      display(data, currentRoom);
     });
   }, 5000);
 
@@ -16,9 +16,20 @@ $(document).ready(function() {
     sendMessage(message);
     $('.sendBox').val('');
   });
+
+  $('.rooms').on('click', '.room', function() {
+    $('.active').removeClass('active');
+    debugger;
+    currentRoom = $(this).text();
+    $(this).addClass('.active');
+  });
+
 });
 
 var server = 'https://api.parse.com/1/classes/chatterbox';
+var rooms = {};
+rooms.all = true;
+var currentRoom = "all";
 
 var sendMessage = function(message) {
   var data = {
@@ -48,16 +59,35 @@ var display = function(messages) {
   var name = '<div class="name"><</div>';
   var text = '<div class="text"></div>';
   var timestamp = '<div class="timestamp"></div>';
+  var room = '<a href= "#" class="room"></a>';
 
   for(var i = 0; i < 100; i++){
+    // populate each individual chat
     var msg = messages[i];
-    var messageToAppend = $(message).append(
-      $(name).text(msg.username + ''),
-      $(text).text(msg.text),
-      $(timestamp).text($.timeago(msg.createdAt))
-    );
-    $('.messages').append(messageToAppend);
+    var messageToAppend;
+    if (currentRoom === "all") {
+      messageToAppend = $(message).append(
+        $(name).text(msg.username),
+        $(text).text(msg.text),
+        $(timestamp).text($.timeago(msg.createdAt))
+      );
+      $('.messages').append(messageToAppend);
+    } else if (msg.roomname && msg.roomname.toLowerCase() === currentRoom) {
+      messageToAppend = $(message).append(
+        $(name).text(msg.username),
+        $(text).text(msg.text),
+        $(timestamp).text($.timeago(msg.createdAt))
+      );
+      $('.messages').append(messageToAppend);
+    }
+    // populate rooms
+    rooms[msg.roomname] = true;
   }
+  // debugger;
+  $('.roomlist').html('');
+  _(rooms).each(function(value, roomName){
+    $('.roomlist').append($(room).text(roomName));
+  });
 };
 
 function getQueryVariable(variable) {
